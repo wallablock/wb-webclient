a6246da9251bc3002ea991c31dfd71f8532e4580
Victor Guardia <victor.guardia@est.fib.upc.edu>
2020-05-13 23:01:25 +0200

    Add JSDoc types to WbConfig

--- src/components/WbConfig.js
+++ src/components/WbConfig.js
@@ -1,6 +1,13 @@
 import DefaultConfig from "../default-config.json";
 import axios from "axios";
 
+/**
+ * @typedef {Object.<string, *>} ConfigLike
+ */
+
+/**
+ * @returns {Promise.<ConfigLike>}
+ */
 async function rawFetchConfig() {
   if (process.env.NODE_ENV === "development") {
     // Won't fail, because it is required at build time
@@ -19,50 +26,91 @@ async function rawFetchConfig() {
   }
 }
 
+/**
+ *
+ * @param {ConfigLike} config
+ * @param {ConfigLike} defaultConfig
+ * @param {string} field
+ * @returns {*}
+ */
 function configGet(config, defaultConfig, field) {
   return config.then((cfg) => cfg[field] ?? defaultConfig[field]);
 }
 
 class ElasticSubConfig {
+  /**
+   * @param {Promise.<ConfigLike>} promisedSubConfig
+   */
   constructor(promisedSubConfig) {
     this.subconfig = promisedSubConfig;
   }
 
+  /**
+   * @private
+   * @param {string} field
+   * @returns {Promise.<*>}
+   */
   cget(field) {
     return configGet(this.subconfig, DefaultConfig.elastic, field);
   }
 
+  /**
+   * @returns {Promise<string>}
+   */
   get url() {
     return this.cget("url");
   }
 
+  /**
+   * @returns {Promise<string>}
+   */
   get key() {
     return this.cget("key");
   }
 }
 
 class Config {
+  /**
+   * @param {Promise.<ConfigLike>} promisedConfig
+   */
   constructor(promisedConfig) {
     this.config = promisedConfig;
   }
 
+  /**
+   * @private
+   * @param {string} field
+   * @returns {Promise.<*>}
+   */
   cget(field) {
     return configGet(this.config, DefaultConfig, field);
   }
 
+  /**
+   * @returns {Promise<string>}
+   */
   get blockchainUrl() {
     return this.cget("blockchainUrl");
   }
 
+  /**
+   * @returns {Promise<string>}
+   */
   get fileproxyUrl() {
     return this.cget("fileproxyUrl");
   }
 
+  /**
+   * @returns {ElasticSubConfig}
+   */
   get elastic() {
     return new ElasticSubConfig(this.config.then((cfg) => cfg.elastic));
   }
 }
 
+/**
+ * @returns {Config}
+ */
 export async function fetchConfig() {
   return new Config(rawFetchConfig());
 }
